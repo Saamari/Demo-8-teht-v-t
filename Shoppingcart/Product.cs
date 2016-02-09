f92aee7000a10010ffe94b61e2f9fd4334293af6
sampsa kautto <sampsa_kautto@hotmail.com>
2016-02-09 05:50:05 +0200

    mo

---
+++ Shoppingcart/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppingcart
+{
+    class Product
+    {
+        public string Milk {get;set;}
+        public string Beer { get; set; }
+
+        public override string ToString()
+        {
+            return Milk + " " + Beer + " " ;
+        }
+    }
+}
---
+++ Shoppingcart/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppingcart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> product = new List<Product>();
+
+            product.Add(new Product { Milk = " Milk 1.40" });
+            product.Add(new Product { Beer = " Beer 0.95" });
+
+            
+            foreach (Product products in product)
+            {
+                Console.WriteLine(products.ToString());
+            }
+        }
+    }
+}
